0286b245ae64f8e0393774024dbd100a8096e543
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-06-10 13:51:26 -0400

    Update suduko-solver.js

--- suduko-solver.js
+++ suduko-solver.js
@@ -21,7 +21,7 @@ let valid = /^[0-9|.]*$/
   if (puzzleString.length != 81) {
     let result = "not 81"
     return result
-    
+  
   }
   }
   
@@ -138,7 +138,9 @@ for (let i = 0; i < 9; i++) {
   }
 
   solve(puzzleString) {
-coordinate = ['a1','a2','a3','a4','a5','a6','a7','a8','a9','b1','b2','b3','b4','b5','b6','b7','b8','b9','c1','c2','c3','c4','c5','c6','c7','c8','c9','d1','d2','d3','d4','d5','d6','d7','d8','d9','e1','e2','e3','e4','e5','e6','e7','e8','e9','f1','f2','f3','f4','f5','f6','f7','f8','f9','g1','g2','g3','g4','g5','g6','g7','g8','g9','h1','h2','h3','h4','h5','h6','h7','h8','h9','i1','i2','i3','i4','i5','i6','i7','i8','i9'] 
+
+    
+let coordinate = ['a1','a2','a3','a4','a5','a6','a7','a8','a9','b1','b2','b3','b4','b5','b6','b7','b8','b9','c1','c2','c3','c4','c5','c6','c7','c8','c9','d1','d2','d3','d4','d5','d6','d7','d8','d9','e1','e2','e3','e4','e5','e6','e7','e8','e9','f1','f2','f3','f4','f5','f6','f7','f8','f9','g1','g2','g3','g4','g5','g6','g7','g8','g9','h1','h2','h3','h4','h5','h6','h7','h8','h9','i1','i2','i3','i4','i5','i6','i7','i8','i9'] 
 
 /*pencilfunction*/
 function pencilArr(puzzleString) {
@@ -152,11 +154,19 @@ if (puzzleString[j] != ".") {
 }
  else {
 for (let i = 1; i <= 9; i++) {
+  console.log(puzzleString)
+  console.log(coordinate[j][0])
+  console.log(coordinate[j][1])
+  console.log(i)
   
-  let pencilRegion = checkRegionPlacement(puzzleString, coordinate[j][0], coordinate[j][1], i)
-let pencilColumn = checkColPlacement(puzzleString, coordinate[j][0], coordinate[j][1], i)
-let pencilRow = checkRowPlacement(puzzleString, coordinate[j][0], coordinate[j][1], i)
-if (pencilRegion != 'truebox' && pencilColumn != 'truecol' && pencilRow != 'truerow') {
+  
+  console.log(this.checkColPlacement(puzzleString,"a", 1, 1))
+   
+let pencilColumn = this.checkColPlacement(puzzleString, coordinate[j][0], coordinate[j][1], i)
+ 
+let pencilRow = this.checkRowPlacement(puzzleString, coordinate[j][0], coordinate[j][1], i)
+let pencilRegion = this.checkRegionPlacement(puzzleString, coordinate[j][0], coordinate[j][1], i)
+if (pencilRegion != 'true' && pencilColumn != 'true' && pencilRow != 'true') {
   pencil[j].push(i)
 }
 }
