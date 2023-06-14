d12eac846a9e0b3ffaf243752c6b69947718e977
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-06-13 22:14:32 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 
-
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
@@ -8,9 +7,14 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
-      
+     
      let puzzleString = req.body.puzzle
     let value = req.body.value
+      
+if (!req.body.coordinate || !req.body.value || !req.body.puzzle){return res.json({ error: 'Required field(s) missing' })
+  
+}
+    
     let row = req.body.coordinate[0].toLowerCase()
     let column = req.body.coordinate[1]
 
@@ -28,12 +32,16 @@ if (result == "missing") {
 if (result == "not 81") {
   return res.json({ error: 'Expected puzzle to be 81 characters long' })
 }
+
 let valCoor = /[a-iA-I]/
 if (valCoor.test(req.body.coordinate[0]) == false) {
   return res.json({ error: 'Invalid coordinate'})
 }
 let valNum = /[1-9]/
 if (valNum.test(req.body.coordinate[1]) == false){
+  return res.json({ error: 'Invalid coordinate'})
+}
+if (req.body.coordinate.length > 2) {
   return res.json({ error: 'Invalid coordinate'})
 }
 if (valNum.test(req.body.value) == false){
@@ -55,8 +63,13 @@ if (colcheck == 'true') {
 if (regioncheck == 'true') {
   conflict.push('region')
 }
+
+      
 if (rowcheck != 'true' && colcheck != 'true' && regioncheck != 'true') {
   return res.json( { "valid": true })
+}
+if (conflict.length == 3){
+return res.json( { "valid": true })
 }
 else {
   return res.json( { "valid": false, "conflict":  conflict  })
